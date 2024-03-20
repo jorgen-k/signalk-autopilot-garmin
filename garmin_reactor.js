2dec0fd5488daf3a2af60b50d5307dcd2b01c695
Jörgen Karlsson <jorgen@Karlsson.com>
2024-03-20 10:52:10 +0100

    First working version

---
+++ garmin_reactor.js
@@ -0,0 +1,117 @@
+
+/*
+ * Copyright 2024 Jörgen Karlsson <jorgen@karlsson.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const successRes = { state: 'SUCCESS' };
+const failureRes = { state: 'FAILURE' };
+
+const generateMsg = (n2kAddr, ccuAddr, message) => `${(new Date()).toISOString()},7,126720,${n2kAddr},${ccuAddr},${message}`;
+const changeHeadingCommand = (code) => `09,E5,98,10,17,04,04,26,${code},00,FF,FF,FF,FF`;
+const changeStateCommand = (code)   => `0B,E5,98,10,17,04,04,05,0A,00,${code},00,FF,FF`;
+const headingCodeMap = new Map([
+  [1, "02"],
+  [15, "03"],
+  [-1, "00"],
+  [-15, "01"],
+]);
+const stateCodeMap = {
+  auto:     `05`,
+  standby:  `02`,
+};
+
+
+module.exports = function(app) {
+  const myN2kAddr = '3'; // TODO: Calculate automatically
+  var ccuAddr = '0';  // Will be discovered
+  var pilot = { id: ccuAddr };
+  var discovered = false;
+  var hwVersion;
+
+  pilot.start = (props) => {
+    ccuAddr = props.deviceId || ccuAddr;
+    pilot.id = ccuAddr;
+    app.debug('Garmin autopilot started:', ccuAddr);
+  };
+
+  pilot.stop = () => {
+    // Placeholder if needed
+  };
+
+  function sendNmea2000(msg) {
+    app.debug("nmea2000out: " + msg);
+    app.emit('nmea2000out', msg);
+    // TODO: Fault handling
+  }
+
+  pilot.putState = (context, path, value, cb) => {
+    var code = stateCodeMap[value];
+    if (code === undefined) {
+      return { message: `Invalid state: ${value}`, ...failureRes };
+    } else {
+      let msg = generateMsg(myN2kAddr, ccuAddr, changeStateCommand(code));
+      sendNmea2000(msg);
+      return successRes;
+    }
+  };
+
+  pilot.putAdjustHeading = (context, path, value, cb) => {
+    const code = headingCodeMap.get(value);
+    if (code === undefined) { 
+      console.log("Unimplemented code for /" + value + "/");
+      return { message: `Invalid adjustment: ${value}`, ...failureRes };
+    } else {
+      sendNmea2000(generateMsg(myN2kAddr, ccuAddr, changeHeadingCommand(code)));
+      return successRes;
+    }
+  };
+
+  pilot.properties = () => {
+    let description = 'No Garmin Reactor found';
+
+    if (!discovered) {
+      const sources = app.getPath('/sources');
+      if (sources) {
+        Object.values(sources).forEach(v => {
+          if (typeof v === 'object') {
+            Object.keys(v).forEach(id => {
+              if (v[id] && v[id].n2k && v[id].n2k.hardwareVersion && v[id].n2k.hardwareVersion.startsWith('Garmin Reactor')) {
+                discovered = true;
+                ccuAddr = id;
+                hwVersion = v[id].n2k.hardwareVersion;
+                description = `Discovered a ${hwVersion} with id ${ccuAddr}`;
+                app.debug(description);
+              }
+            });
+          }
+        });
+      }
+    }
+
+    description = discovered ? `Discovered a ${hwVersion} with address ${ccuAddr}` : `No CCU discovered, using address ${ccuAddr}`;
+    app.debug(description);
+    pilot.id = ccuAddr;     
+    return {
+      deviceId: {
+        type: "string",
+        title: "Garmin NMEA2000 ID",
+        description: description,
+        default: ccuAddr
+      }
+    };
+  };
+
+  return pilot;
+};
---
+++ index.js
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2024 Jörgen Karlsson <jorgen@karlsson.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const modules = {
+  garmin: require('./garmin_reactor')
+};
+
+
+module.exports = function (app) {
+  var plugin = {};
+  var autopilot;
+
+
+  plugin.start = function (props) {
+    const type = props.type;
+    app.debug(`Initializing autopilot of type ${type}...`);
+    if (!modules[type]) {
+      app.error(`Autopilot type '${type}' is not recognized.`);
+      return;
+    }
+    try {
+      const initModule = modules[type];
+      if (typeof initModule !== 'function') {
+        throw new Error(`Module for type '${type}' does not export a function.`);
+      }
+      autopilot = initModule(app);
+      if (!autopilot || typeof autopilot.start !== 'function') {
+        throw new Error(`Module for type '${type}' did not initialize correctly.`);
+      }
+
+      app.debug(`Starting autopilot of type '${type}'`);
+      autopilot.start(props);
+      app.registerPutHandler('vessels.self', "steering.autopilot.state", autopilot.putState);
+      app.registerPutHandler('vessels.self', "steering.autopilot.actions.adjustHeading", autopilot.putAdjustHeading);
+      /*  TODO in future the following put handlers could be added:  
+      targetHeadingPath = "steering.autopilot.target.headingMagnetic";
+      targetWindPath = "steering.autopilot.target.windAngleApparent";
+      tack = "steering.autopilot.actions.tack";
+      advance = "steering.autopilot.actions.advanceWaypoint";
+      */
+    } catch (error) {
+      app.error(`Failed to initialize autopilot type '${type}': ${error.message}`);
+    }
+  };
+
+  plugin.stop = function () {
+    if (autopilot) {
+      autopilot.stop();
+    }
+  };
+
+  plugin.id = "autopilot-garmin";
+  plugin.name = "Autopilot Garmin Control";
+  plugin.description = "Plugin that controls a Garmin Reactor autopilot";
+
+  plugin.schema = function () {
+    let config = {
+      title: "Autopilot Control",
+      type: "object",
+      properties: {
+        type: {
+          type: 'string',
+          title: 'Autopilot Type',
+          enum: [
+            'garmin'
+          ],
+          enumNames: [
+            'Garmin Reactor'
+          ],
+          default: 'garmin'
+        }
+      }
+    };
+
+    Object.values(pilots).forEach(autopilot => {
+      if (autopilot && autopilot.properties) {
+        config.properties = { ...autopilot.properties(), ...config.properties };
+      }
+    });
+    return config;
+  };
+
+  return plugin;
+};
+
